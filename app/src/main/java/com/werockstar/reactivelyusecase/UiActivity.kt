a660b12bb5e6752c99d0e00b6f208b0dc11e154b
WeRockStar <kotchaphan.m@gmail.com>
2019-08-25 18:00:55 +0700

    Add more example

--- app/src/main/java/com/werockstar/reactivelyusecase/UiActivity.kt
+++ app/src/main/java/com/werockstar/reactivelyusecase/UiActivity.kt
@@ -4,7 +4,9 @@ package com.werockstar.reactivelyusecase
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.jakewharton.rxbinding3.view.clicks
+import com.jakewharton.rxbinding3.widget.afterTextChangeEvents
 import kotlinx.android.synthetic.main.activity_ui.*
+import java.util.concurrent.TimeUnit
 
 class UiActivity : AppCompatActivity(R.layout.activity_ui) {
 
@@ -12,6 +14,13 @@ class UiActivity : AppCompatActivity(R.layout.activity_ui) {
         super.onCreate(savedInstanceState)
 
         button.clicks()
+            .debounce(150, TimeUnit.MILLISECONDS)
             .subscribe()
+
+        editText.afterTextChangeEvents()
+            .debounce(150, TimeUnit.MILLISECONDS)
+            .distinctUntilChanged()
+            .subscribe()
+
     }
 }
