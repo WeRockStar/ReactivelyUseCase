3e1127f867538257df5f0579345ede51ec4fb323
WeRockStar <kotchaphan.m@gmail.com>
2019-08-25 16:31:27 +0700

    Prepare activity for basic rx for UI

--- app/src/main/java/com/werockstar/reactivelyusecase/MainActivity.kt
+++ app/src/main/java/com/werockstar/reactivelyusecase/MainActivity.kt
@@ -1,5 +1,17 @@
 package com.werockstar.reactivelyusecase
 
+import android.content.Intent
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        buttonUI.setOnClickListener {
+            startActivity(Intent(this, UiActivity::class.java))
+        }
+    }
+}
