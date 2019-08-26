42383e0ad31263192bb013f1dff9a0c9333d8fca
WeRockStar <kotchaphan.m@gmail.com>
2019-08-26 21:50:34 +0700

    Add retry patterns

--- app/src/main/java/com/werockstar/reactivelyusecase/UiActivity.kt
+++ app/src/main/java/com/werockstar/reactivelyusecase/UiActivity.kt
@@ -1,15 +1,23 @@
 package com.werockstar.reactivelyusecase
 
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.jakewharton.rxbinding3.view.clicks
 import com.jakewharton.rxbinding3.widget.afterTextChangeEvents
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.Observables
 import kotlinx.android.synthetic.main.activity_ui.*
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
 
 class UiActivity : AppCompatActivity(R.layout.activity_ui) {
 
+    private val api by lazy { API() }
+
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -22,5 +30,24 @@ class UiActivity : AppCompatActivity(R.layout.activity_ui) {
             .distinctUntilChanged()
             .subscribe()
 
+        editText.afterTextChangeEvents()
+            .skipInitialValue()
+            .debounce(200, TimeUnit.MILLISECONDS)
+            .distinctUntilChanged()
+            .map { it.editable.toString() }
+            .switchMap {
+                api.getResult(it)
+                    .retry { attempt, throwable ->
+                        throwable is TimeoutException
+                                && attempt <= 2
+                    }
+            }
+            .subscribe()
+    }
+
+    class API {
+        fun getResult(keyword: String): Observable<String> {
+            return Observable.error(TimeoutException())
+        }
     }
 }
